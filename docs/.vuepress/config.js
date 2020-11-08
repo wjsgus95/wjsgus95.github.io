5b59d301bd7b782ac52127affa4284bbef1d0953
Hyunjun <wjsgus95@gmail.com>
2020-11-09 01:24:50 +0900

    somehow I'm an early adopter of new version of google analytics so I gotta use gtag

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,74 +1,84 @@
 module.exports = {
-    title: 'June\'s blog',
-    // Markdown config
-    markdown: {
-        lineNumbers: true
-    },
-    // Theme config
-    theme: 'vuepress-theme-meteorlxy',
-    themeConfig: {
-        lang: 'en-US',
-        nav: [
-            { text: 'Home', link: '/', exact: true },
-            { text: 'About', link: '/about/', exact: false },
-            { text: 'CV', link: '/cv/', exact: false },
-            { text: 'Github', link: 'https://www.github.com/wjsgus95' },
-        ],
-        personalInfo: {
-            nickname: 'june',
-            description: '(wanna be) jr backend devloper',
-            email: 'wjsgus95@yonsei.ac.kr',
-            location: 'South Korea',
-            organization: 'Yonsei University',
-            avatar: '/img/profile.jpg',
+  title: 'June\'s blog',
+  // Markdown config
+  markdown: {
+	lineNumbers: true
+  },
+  // Theme config
+  theme: 'vuepress-theme-meteorlxy',
+  themeConfig: {
+	lang: 'en-US',
+	nav: [
+	  { text: 'Home', link: '/', exact: true },
+	  { text: 'About', link: '/about/', exact: false },
+	  { text: 'CV', link: '/cv/', exact: false },
+	  { text: 'Github', link: 'https://www.github.com/wjsgus95' },
+	],
+	personalInfo: {
+	  nickname: 'june',
+	  description: '(wanna be) jr backend devloper',
+	  email: 'wjsgus95@yonsei.ac.kr',
+	  location: 'South Korea',
+	  organization: 'Yonsei University',
+	  avatar: '/img/profile.jpg',
 
-            sns: {
-                github: {
-                    account: 'wjsgus95',
-                    link: 'https://github.com/wjsgus95',
-                },
-                linkedin: {
-                    account: 'wjsgus95',
-                    link: 'https://www.linkedin.com/in/hyunjun-jeon-89182310a/',
-                },
-            },
-        },
+	  sns: {
+		github: {
+		  account: 'wjsgus95',
+		  link: 'https://github.com/wjsgus95',
+		},
+		linkedin: {
+		  account: 'wjsgus95',
+		  link: 'https://www.linkedin.com/in/hyunjun-jeon-89182310a/',
+		},
+	  },
+	},
 
-        footer: {
-            poweredByTheme: false,
-        },
-        // Show the last updated time of your posts
-        lastUpdated: true,
-        // Pagination config (Optional)
-        pagination: {
-            perPage: 10,
-        },
+	footer: {
+	  poweredByTheme: false,
+	},
+	// Show the last updated time of your posts
+	lastUpdated: true,
+	// Pagination config (Optional)
+	pagination: {
+	  perPage: 10,
+	},
 
-        // Comments Config
-        comments: {
-            platform: 'github', // Optional, default is 'github'. You can also choose 'gitlab', 'bitbucket'. Check Vssue docs for details.
-            owner: 'wjsgus95',
-            repo: 'wjsgus95.github.io',
-            clientId: '83e57b33578d7e181898',
-            clientSecret: 'ea2578531c64721d7a6ef15a111dde6fcbc20c2c',
-            autoCreateIssue: process.env.NODE_ENV !== 'development', // Optional, this will not create issue autoly in development mode
-        },
-    },
-    // Plugins
-    plugins: [
-        [
-            '@vuepress/blog',
-            {
-                sitemap: {
-                    hostname: 'https://wjsgus95.github.io',
-                },
-            },
-        ],
-				[
-						'@vuepress/google-analytics',
-						{
-							'ga': 'UA-G-1235YFPJGC',
-						},
-				],
-    ],
+	// Comments Config
+	comments: {
+	  platform: 'github', // Optional, default is 'github'. You can also choose 'gitlab', 'bitbucket'. Check Vssue docs for details.
+	  owner: 'wjsgus95',
+	  repo: 'wjsgus95.github.io',
+	  clientId: '83e57b33578d7e181898',
+	  clientSecret: 'ea2578531c64721d7a6ef15a111dde6fcbc20c2c',
+	  autoCreateIssue: process.env.NODE_ENV !== 'development', // Optional, this will not create issue autoly in development mode
+	},
+  },
+  // index.html head props
+  head: [
+	[
+	  'script', {
+		async: true,
+		src: 'https://www.googletagmanager.com/gtag/js?id=G-1235YFPJGC'
+	  }
+	],
+	['script', {}, `
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-1235YFPJGC');
+	  ` ],
+  ],
+  // Plugins
+  plugins: [
+	[
+	  '@vuepress/blog',
+	  {
+		sitemap: {
+		  hostname: 'https://wjsgus95.github.io',
+		},
+	  },
+	],
+  ],
 }
